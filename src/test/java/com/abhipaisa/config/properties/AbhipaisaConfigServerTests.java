28623d471cdbb9beb8da6d418b00ca1b0b13c4c4
unknown <prasad.b@TS-PCPrasad.tekrains.com>
2018-01-09 17:18:30 +0530

    fielsaddede

---
+++ src/test/java/com/abhipaisa/config/properties/AbhipaisaConfigServerTests.java
@@ -0,0 +1,16 @@
+package com.abhipaisa.config.properties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbhipaisaConfigServerTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
